0f43f6c2de631c10a4c61f51bd74fde42f0a4db3
Lahari-01 <kollurulahari99@gmail.com>
2020-06-13 12:23:51 +0530

    added spring project

--- SpringSORTApplication/src/test/java/com/mthree/SpringSortApplicationTests.java
+++ SpringSORTApplication/src/test/java/com/mthree/SpringSortApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mthree.springsortapplication;
+package com.mthree;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringSortApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
